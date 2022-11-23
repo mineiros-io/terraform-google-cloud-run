93ab7bcd02fe88d1c6255ac79a5a2ab42e8623eb
Bence Wax <bence.wax@mineiros.io>
2022-11-23 18:22:34 +0100

    chore: update from module template

--- test/unit-complete/main.tf
+++ test/unit-complete/main.tf
@@ -3,20 +3,109 @@ module "test" {
 
   # add all required arguments
 
-  name     = "test-${local.random_suffix}"
+  name     = "unit-complete-${local.random_suffix}"
   location = "europe-west3"
+  project  = local.project_id
 
-  project = local.project_id
+  autogenerate_revision_name = true
 
   template = {
+    metadata = {
+      name      = "unit-complete-meta"
+      namespace = "unit-complete-ns"
+      lables = {
+        foo = "bar"
+      }
+      generation       = 0
+      resource_version = "0.0.0"
+      self_link        = "path/to/self"
+      uid              = "fd32528f-8020-4f04-9e5e-c89ea4f904dd"
+      annotations = {
+        foo = "bar"
+      }
+    }
     spec = {
+      conatiner_concurrency = 0
+      timeout_seconds       = 100
+      service_account_name  = "unit-complete-sa"
+      serving_state         = "ready"
       containers = [
         {
-          image = "gcr.io/cloudrun/hello:latest"
+          args    = ["/bin/sh", "-c"]
+          image   = "gcr.io/cloudrun/hello:latest"
+          command = ["yes"]
+          env = {
+            name  = "unit-complete"
+            value = "foo"
+            value_from = {
+              secret_key_ref = {
+                key  = "latest"
+                name = "unit-complete"
+              }
+            }
+          }
+          ports = [
+            {
+              name           = "unit-complete-port"
+              protocol       = "TCP"
+              container_port = 42069
+            }
+          ]
+          resources = {
+            limits = {
+              cpu    = "250m"
+              memory = "1Gi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "1Gi"
+            }
+          }
         }
       ]
     }
   }
+
+  traffic = [
+    {
+      revision_name   = "first"
+      percent         = 69
+      latest_revision = false
+
+      metadata = {
+        namespace = "unit-complete-ns"
+        labels = {
+          foo = "bar"
+        }
+        generation       = 0
+        resource_version = "0.0.0"
+        self_link        = "path/to/self"
+        uid              = "fd32528f-8020-4f04-9e5e-c89ea4f904dd"
+        annotations = {
+          foo = "bar"
+        }
+      }
+      domain_mapping = {
+        spec = {
+          force_override   = true
+          certificate_mode = "NONE"
+        }
+        metadata = {
+          namespace = "unit-complete-ns"
+          labels = {
+            foo = "bar"
+          }
+          generation       = 0
+          resource_version = "0.0.0"
+          self_link        = "path/to/self"
+          uid              = "fd32528f-8020-4f04-9e5e-c89ea4f904dd"
+          annotations = {
+            foo = "bar"
+          }
+        }
+      }
+    }
+  ]
 
   # add all optional arguments that create additional/extended resources
 
